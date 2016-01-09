c08bc0eb026e32adcda6e00e5ffff4a33901bcd9
Abigail <abilynsmith@yahoo.com>
2016-01-09 09:56:16 -0800

    Finished the pcb.h file.  It may have some issues, I'm still rusty with c!  But all the method definitions are there, so whoever's doing the test should be able to get started.

--- Problem 1/pcb.h
+++ Problem 1/pcb.h
@@ -3,14 +3,37 @@
  *
  *  Created on: Jan 7, 2016
  *      Author: Sean
+ *      Contributor: Abigail
  */
 
 #ifndef PCB_H_
 #define PCB_H_
 
 typedef struct pcb {
-	int *content;
+	int priority;
+	int ID;
+	int next;
 } PCB;
 typedef PCB * PCB_p;
+
+/**
+ * Sets a new priority for this PCB
+ */
+void setPriority(int * priority);
+
+/**
+ * Returns the value of the priority of this PCB
+ */
+int getPriority();
+
+/**
+ * Returns a string representation of this PCB.
+ */
+const char *toString();
+
+/**
+ * Deallocates all memory references that are kept within this PCB.
+ */
+void Destroy();
 
 #endif /* PCB_H_ */
