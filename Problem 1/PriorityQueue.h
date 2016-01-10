5db77d482e52e5166be58feed5710e9fb92f3720
wsp93 <wsp93@users.noreply.github.com>
2016-01-09 22:38:03 -0800

    Made more "Object-Oriented"
    Added: Constructor, Destructor, necessary #includes
    Fixed functions to take a PriorityQPtr parameter

--- Problem 1/PriorityQueue.h
+++ Problem 1/PriorityQueue.h
@@ -1,32 +1,41 @@
 // Author: Wing-Sea Poon
 
 #ifndef PRIORITY_QUEUE_H
-
 #def PRIORITY_QUEUE_H
 
-#define PRIORITY_LEVELS = 16;
+#include <stdbool.h>
+#include "Fifo.h"
+#include "Pcb.h"
+
+#define PRIORITY_LEVELS 16
 
 // Priority Queue ADT
 typedef struct 
 {
-	FIFO priority_array[PRIORITY_LEVELS];
-} Priority_Str;
-typedef Priority_Str* Priority_Ptr;
+	FIFO priorityArray[PRIORITY_LEVELS];
+} PriorityQStr;
+typedef PriorityQStr* PriorityQPtr;
+
+// Allocates memory for a Priority Queue object.
+PriorityQPtr priorityQConstructor();
+
+// Deallocates memory for a Priority Queue object.
+void priorityQDestructor(PriorityQPtr this);
 
 // Adds the PCB to the end of a FIFO queue based on the PCB's priority.
-void enqueue(PCB* pcb);
+void enqueue(PriorityQPtr this, PCB* pcb);
 
 // Removes and returns the PCB with the highest priority.
-PCB* dequeue();
+PCB* dequeue(PriorityQPtr this);
 
 // Returns, without removing, the PCB with the highest priority.
-PCB* peek();
+PCB* peek(PriorityQPtr this);
 
 // Checks whether there are any PCBs in this priority queue.
-bool isEmpty();
+bool isEmpty(PriorityQPtr this);
 
 // Returns a string representation of this priority queue.
-char* toString();
+char* toString(PriorityQPtr this);
 
 #endif
 
