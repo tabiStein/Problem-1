af6afa7e35020a92de6b3908ea1c44196ad8d9d0
Abigail Smith <abilynsmith@yahoo.com>
2016-01-12 20:25:02 -0800

    Finished test
    The test is finished.  There's something going on with one of the toStrings, I'm thinking the PCB toString.  I can run this test just fine in my debugger, but when I try to run it normally, I get a crash.  I think I've narrowed it down to somewhere in the PCB toString (with the extensive use of printfs!), but I don't know what it is.  I think it's some kind of memory/stack breach, based on what I've read online.

--- Problem 1/priorityQueueTest.c
+++ Problem 1/priorityQueueTest.c
@@ -9,23 +9,35 @@
 int main() {
     PQPtr pQ = pqConstructor();
 
-    //create 10 pcbs with a random priority, ids from 1-10?
+    //create 100 pcbs with a random priority, and assigns them an ids from 1-100.
+    //after 10 pcbs have been created and enqueued, pause the program to print the results.
+    //after each group of 10, dequeue from 4 to 6 PCBs, print out the contents of the dequeued PCBs, then reprint the PQ so far.
+    
     //enqueue each pcb, print as they are enqueued
     srand( time(NULL) );
     PcbPtr pcb;
+    int id = 1;
     int i;
     for (i = 0; i < 10; i++) {
-        //printf("%s\n", pqToString(pQ));
-        //printf("%d\n", i);
-        pcb = newPCB();
-        setPriority(pcb, rand() % 15);
-
-        printf("%d\n", pcb->priority);
-
-        setID(pcb, i + 1);
-        pqEnqueue(pQ, pcb);
+        int j;
+        for (j = 0; j < 10; j++) {
+            pcb = newPCB();
+            setPriority(pcb, rand() % 16);
+            setID(pcb, id++);
+            pqEnqueue(pQ, pcb);
+        }
         //print out the priority queue so far
-        printf("%s\n", pqToString(pQ));
+        printf("After enqueueing 10 PCBs:\n%s\n", pqToString(pQ));
+        //pause the program to take a screencap
+
+        //dequeue 4 to 6 PCBs:
+        int dequeue = (rand() % 3) + 4;
+        int k;
+        for (k = 0; k < dequeue; k++) {
+            PcbPtr dpcb = pqDequeue(pQ);
+            printf("A dequeued PCB: %s\n", toString(dpcb));
+        }
+        printf("After dequeueing %d PCBs:\n%s\n\n", dequeue, pqToString(pQ));
     }
     return 0;
 }
