05e8bd7fa82aa757cb50d5208180bd29adae8c21
Abigail Smith <abilynsmith@yahoo.com>
2016-01-15 12:27:20 -0800

    Update pcbTest.c
    just a slight update to the test.

--- Problem 1/pcbTest.c
+++ Problem 1/pcbTest.c
@@ -9,6 +9,22 @@
 //#include <stdio.h>
 //#include <time.h>
 //
+
+//Abby's updated test
+int main(void) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	//PcbStr *randPcb = malloc(sizeof(PcbStr));
+	PcbStr *randPcb = newPCB();
+	printf("PCB Before defining members:\n %s\n", toString(randPcb));
+	srand( time(NULL) );
+	setPriority(randPcb, rand() % 16);
+	setID(randPcb, rand());
+	printf("Press enter to see PCB after members have been randomly defined:\n");
+	getchar();
+	printf("%s\n", toString(randPcb));
+	return 0;
+}
+
 //int main(void) {
 //	setvbuf(stdout, NULL, _IONBF, 0);
 //	PcbStr *randPcb = malloc(sizeof(PcbStr));
