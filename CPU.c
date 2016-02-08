7b02fc743cedacb7915b8f2b0cc58a2ccd63530c
Abigail Smith <abilynsmith@yahoo.com>
2016-02-08 13:33:12 -0800

    Update CPU.c
    Update the main loop.  Also added a terminate ISR with a TERMINATE_INTERRUPT field to handle processes being terminated.

--- CPU.c
+++ CPU.c
@@ -22,6 +22,7 @@
 
 #define TIMER 1
 #define IO 2
+#define IO_INTERRUPT 3
 #define NEW_PROCS 5
 #define PRIORITY_LEVELS 16
 #define ROUNDS_TO_PRINT 4 // the number of rounds to wait before printing simulation data
@@ -80,12 +81,23 @@ void scheduler(int interruptType, int numIO) {
 		//set new io waiting queue process to running
 		currProcess = pcb;
 		PCBSetState(currProcess, running);
+	} else if (interruptType == TERMINATE_INTERUPT) {
+		fifoQueueEnqueue(terminatedProcesses, currProcess);
+		PCBSetState(currProcess, terminated);
+		currProcess->TERMINATION = (double)clock();
+		dispatcher();
 	}
 }
 
 /*Saves the state of the CPU to the currently running PCB.*/
 void saveCpuToPcb() {
 	PCBSetPC(currProcess, sysStackPC);
+}
+
+void terminateIsr() {
+	//save cpu to pcb??
+	PCBSetState(currProcess, interrupted);
+	scheduler(TERMINATE_INTERRUPT, -1);
 }
 
 /*The interrupt service routine for a IO interrupt
@@ -125,23 +137,22 @@ void genProcesses() {
 	}
 }
 
-///*Writes the given string to the given file*/
-//void writeToFile(FILE* filePtr, const char* string) {
-//  fprintf(filePtr, "%s", string);
-//}
 int main(void) {
 	srand(time(NULL));
 	outFilePtr = fopen("scheduleTrace.txt", "w");
 	currPID = 0;
 	sysStackPC = 0;
-	timerCount = 0;
-	unsigned int PCRegister;
+	io1Count = -1;
+	io2Count = -1;
+	timerCount = TIMER_QUANTUM;
+	//unsigned int PCRegister;
 	newProcesses = fifoQueueConstructor();
 	readyProcesses = fifoQueueConstructor();
 	terminatedProcesses = fifoQueueConstructor();
 	waitQueue1 = fifoQueueConstructor();
 	waitQueue2 = fifoQueueConstructor();
 
+	/*
 	//An initial process to start with
 	currProcess = PCBConstructor();
 	PCBSetPC(currProcess, rand());
@@ -149,23 +160,53 @@ int main(void) {
 	PCBSetPriority(currProcess, rand() % PRIORITY_LEVELS);
 	PCBSetState(currProcess, running);
 	currPID++;
-	PCRegister = currProcess->PC;
+	//PCRegister = currProcess->PC;
+
+	 */
+
+	printf("Generating Processes:\n");
 
 	genProcesses();
+	int i;
+	for (i = 0; i < newProcesses->size; i++) {
+		PcbPtr pcb = fifoQueueDequeue(newProcesses);
+		PCBSetState(pcb, ready);
+		fifoQueueEnqueue(readyProcesses, pcb);
+		//fprintf(outFilePtr, "%s\r\n", PCBToString(pcb));
+	}
+	dispatcher();
+
+	printf("Begin Simulation:\n");
+	printf("Currently running process: %s\n\n", PCBToString(currProcess));
 
 	int simCounter = 0;
 	while (simCounter <= SIMULATION_END) {
 		//check for timer interrupt, if so, call timerISR()
 		if (timerCheck() == 1) {
-			timerISR();
+			printf("A timer interrupt has occurred.\n");
+
+			timerIsr();
+
+			printf("readyProcesses current state: %s\n", fifoQueueToString(readyProcesses));
+			printf("Currently running process: %s\n\n", PCBToString(currProcess));
 		}
 
 		//check if there has been an IO interrupt, if so call appropriate ioISR
 		if (io1Check() == 1) {
-			//do something
+			printf("I/O 1 device has thrown an interrupt.\n");
+
+			//call the IO service routine
+			IO_ISR(1);
+
+			printf("waitQueue1 current state: %s\n\n", fifoQueueToString(waitQueue1));
 		}
 		if (io2Check() == 1) {
-			//do something
+			printf("I/O 2 device has thrown an interrupt.\n");
+
+			//call the IO service routine
+			IO_ISR(2);
+
+			printf("waitQueue2 current state: %s\n\n", fifoQueueToString(waitQueue2));
 		}
 
 		//check the current process's PC, if it is MAX_PC, set to 0 and increment TERM_COUNT
@@ -174,7 +215,15 @@ int main(void) {
 
 			//if TERM_COUNT = TERMINATE, then call terminateISR to put this process in the terminated list
 			if (currProcess->TERM_COUNT == currProcess->TERMINATE) {
-				terminateISR();
+				printf("process being terminated\n");
+
+				terminateIsr();
+
+				printf("Terminated process: %s\n", PCBToString(currProcess));
+				printf("terminatedProcesses current state: %s\n", fifoQueueToString(terminatedProcesses));
+				printf("readyProcesses current state: %s\n", fifoQueueToString(readyProcesses));
+				printf("Currently running process: %s\n\n", PCBToString(currProcess));
+
 				continue;	//currProcess has been terminated, we don't want to execute the rest of the loop, instead jump to next iteration
 			}
 			currProcess->PC = 0;
@@ -188,19 +237,31 @@ int main(void) {
 				currProcess->PC == currProcess->IO_1_TRAPS[1] ||
 				currProcess->PC == currProcess->IO_1_TRAPS[2] ||
 				currProcess->PC == currProcess->IO_1_TRAPS[3]) {
-			IO_ISR(1);
+
+			printf("I/O 1 request\n");
+
+			trapServiceHandler(1);
+
+			printf("waitQueue1 current state: %s\n", fifoQueueToString(waitQueue1));
+			printf("Currently running process: %s\n\n", PCBToString(currProcess));
 		} else if (currProcess->PC == currProcess->IO_2_TRAPS[0] ||
 				currProcess->PC == currProcess->IO_2_TRAPS[1] ||
 				currProcess->PC == currProcess->IO_2_TRAPS[2] ||
 				currProcess->PC == currProcess->IO_2_TRAPS[3]) {
-			IO_ISR(2);
-		}
 
+			printf("I/O 2 request\n");
+
+			trapServiceHandler(2);
+
+			printf("waitQueue2 current state: %s\n", fifoQueueToString(waitQueue2));
+			printf("Currently running process: %s\n\n", PCBToString(currProcess));
+		}
 
 		//at end
 		simCounter++;
 	}
 
+	printf("End of simulation\n");
 	return 0;
 }
 
